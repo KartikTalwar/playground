3cd795cc54f6640c16a24102326a8b8519fbb979
Ubuntu 11.10 <ktalwar@ubuntu.(none)>
2012-01-09 20:02:25 -0800

    Basics

---
+++ perl/01Basics.pl
@@ -0,0 +1,24 @@
+#!usr/bin/perl
+
+print "Hello, World!\n";
+
+$astring = "Oh, hi!\n";
+print $astring;
+
+$bstring = q/you can do this too/; 
+print $bstring;
+print "\n";
+
+
+$anotherOne = <<"EOF";
+This is something I can do to write strings as well
+EOF
+
+print $anotherOne;
+print "\n";
+
+
+# $string = q[you can do this too]; 
+# $string = q(you can do this too); 
+# $string = q{you can do this too}; 
+# $string = q<you can do this too>; 
