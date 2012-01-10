0cf85ab35e2c3d30db5458e75f6eddd63dd9a251
Kartik Talwar <talwar.kartik@gmail.com>
2012-01-09 20:29:48 -0800

    Basic Math

---
+++ perl/03Math.pl
@@ -0,0 +1,28 @@
+#!usr/bin/perl
+
+$a = 42;
+$b = 23;
+
+print $a + $b;
+print "\n";
+
+print $a * $b;
+print "\n";
+
+print $a / $b;
+print "\n";
+
+print $a % $b;
+print "\n";
+
+print $a - $b;
+print "\n";
+
+$a *= 11;
+print $a;
+print "\n";
+
+print $b++;
+print "\n";
+
+
