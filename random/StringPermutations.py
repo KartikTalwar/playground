70b3e26cd26829ff66f9a9e1926dc790609ee81f
Kartik Talwar <ktalwar@tagged.com>
2012-01-19 11:12:00 -0800

    String Permutations

---
+++ random/StringPermutations.py
@@ -0,0 +1,14 @@
+
+def stringPermutations(string):
+	rez = []
+	if len(string) < 2:
+		rez.append(string)
+	else:
+		for position in range(len(string)):
+			perms = string[:position] + string[position+1:]
+			for i in stringPermutations(perms):
+				rez.append(string[position:position+1] + i)
+
+	return rez
+
+print stringPermutations('abc')
