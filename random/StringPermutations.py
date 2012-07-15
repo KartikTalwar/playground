9876e64660ddc46245397f2c7761732d7d74ad02
Kartik <hi@kartikt.com>
2012-07-14 22:57:02 -0300

    Better Indentation on StringPermutations

--- random/StringPermutations.py
+++ random/StringPermutations.py
@@ -1,14 +1,14 @@
-
 def stringPermutations(string):
-	rez = []
-	if len(string) < 2:
-		rez.append(string)
-	else:
-		for position in range(len(string)):
-			perms = string[:position] + string[position+1:]
-			for i in stringPermutations(perms):
-				rez.append(string[position:position+1] + i)
+    rez = []
 
-	return rez
+    if len(string) < 2:
+        rez.append(string)
+    else:
+        for position in range(len(string)):
+            perms = string[:position] + string[position+1:]
+            for i in stringPermutations(perms):
+                rez.append(string[position:position+1] + i)
 
-print stringPermutations('abc')
+    return rez
+
+print stringPermutations('abc') # ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
